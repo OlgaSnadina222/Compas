5d8d552dbf69c1b62bf5deb81e1a73e7b4f0d9d9
Snadina Olga <snadinao@gmail.com>
2021-12-06 15:38:42 +0300

    Compas
    ready

---
+++ app/src/main/java/com/example/compas/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.compas
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity: AppCompatActivity(), SensorEventListener {
+
+    var manager: SensorManager? = null
+    private var currentDegree: Int = 0
+    var tvDegree: TextView? = null
+    private var imDinamic:ImageView?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        manager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        tvDegree = findViewById(R.id.tvDegree)
+        imDinamic = findViewById(R.id.imDinamic)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        manager?.registerListener(this, manager?.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        manager?.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        val degree: Int = event?.values?.get(0)?.toInt()!!
+        tvDegree?.text = degree.toString()
+        val rotationAnim = RotateAnimation(currentDegree.toFloat(),(-degree).toFloat(),
+            Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+        rotationAnim.duration = 210
+        rotationAnim.fillAfter = true
+        currentDegree = -degree
+        imDinamic?.startAnimation(rotationAnim)
+
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+}
